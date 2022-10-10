16b85bf4af8f99240cfafebb111f4bc478c8c2e9
kazizi55 <kaz.redhot1201@gmail.com>
2022-10-10 20:45:47 +0900

    feat: stream comment

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,8 +1,25 @@
+import { injectComment } from "./injectComment";
+
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   switch (request.method) {
     case "setItem":
       chrome.storage.local.set({
         googleMeetCommentFlow: request.value,
+      });
+      return true;
+    case "injectCommentToAllTabs":
+      chrome.storage.local.get(["googleMeetCommentFlow"]).then((res) => {
+        chrome.tabs.query({}, (tabs) => {
+          tabs.forEach((tab) => {
+            if (!tab.id) return;
+
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id },
+              func: injectComment,
+              args: [res["googleMeetCommentFlow"]],
+            });
+          });
+        });
       });
       return true;
     default:
