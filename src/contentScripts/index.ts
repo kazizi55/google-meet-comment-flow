4d08e781ea2d7cbc68685f40f362b2635070aa19
kazizi55 <kaz.redhot1201@gmail.com>
2022-10-10 14:50:18 +0900

    refactor: index.ts

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,38 +1,40 @@
 // FIXME
 export {};
 
-let prevThread: any;
+let prevThread: Node;
 
 const CLASS_OBJ = {
   thread: "z38b6",
-  messages: "oIy2qc",
-};
+  message: "oIy2qc",
+  isLoading: "gYckH",
+} as const;
 
-let observer = new MutationObserver((records) => {
+const observer = new MutationObserver(() => {
   try {
     const thread = document.getElementsByClassName(CLASS_OBJ.thread)[0];
 
-    if (prevThread != undefined && thread.isEqualNode(prevThread)) return;
-    if (thread.getElementsByClassName("gYckH").length == 1) return;
+    if (prevThread !== undefined && thread.isEqualNode(prevThread)) return;
+    if (thread.getElementsByClassName(CLASS_OBJ.isLoading).length === 1) return;
 
     prevThread = thread.cloneNode(true);
-    const messages = thread.getElementsByClassName(CLASS_OBJ.messages);
+
+    const messages = thread.getElementsByClassName(CLASS_OBJ.message);
     const message = messages[messages.length - 1].innerHTML;
 
-    let screen = document.body;
-    let screenHeight = screen.offsetHeight;
-    let screenWidth = screen.offsetWidth;
+    const screen = document.body;
+    const screenHeight = screen.offsetHeight;
+    const screenWidth = screen.offsetWidth;
 
-    let comment = document.createElement("span");
+    const comment = document.createElement("span");
 
     comment.textContent = message;
-    document.getElementsByTagName("body")[0].appendChild(comment);
+    document.body.appendChild(comment);
 
-    let letterSize = screenHeight * 0.05 * 1;
+    const letterSize = screenHeight * 0.05 * 1;
     comment.setAttribute("class", "comment");
 
     const footerHeight = 88;
-    let topPosition = Math.floor(
+    const topPosition = Math.floor(
       (screenHeight - letterSize - footerHeight) * Math.random()
     );
     const commentStyle = {
@@ -60,17 +62,13 @@ let observer = new MutationObserver((records) => {
       }
     );
   } catch (e) {
-    return;
+    console.error(e);
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  let elem = document.body;
-
-  const OBSERVE_CONFIG = {
+document.addEventListener("DOMContentLoaded", () =>
+  observer.observe(document.body, {
     attributes: true,
     subtree: true,
-  };
-
-  observer.observe(elem, OBSERVE_CONFIG);
-});
+  })
+);
