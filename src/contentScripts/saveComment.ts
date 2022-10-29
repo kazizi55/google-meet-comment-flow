197546ed648b3ab922b603afed430b2dfa7c02a4
kazizi55 <kaz.redhot1201@gmail.com>
2022-10-29 16:25:29 +0900

    perf: change element specification method

--- src/contentScripts/saveComment.ts
+++ src/contentScripts/saveComment.ts
@@ -1,25 +1,28 @@
 let prevThread: Node;
 
-const CLASS_OBJ = {
-  thread: "z38b6",
-  message: "oIy2qc",
-  isLoading: "gYckH",
+const SELECTOR_OBJ = {
+  thread:
+    "#ow3 > div.T4LgNb > div > div:nth-child(12) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6",
+  message:
+    "#ow3 > div.T4LgNb > div > div:nth-child(12) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6 > div > div.Zmm6We > div",
+  isLoading:
+    "#ow3 > div.T4LgNb > div > div:nth-child(12) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6 > div > div.Zmm6We > div.gYckH",
 } as const;
 
 const observer = new MutationObserver(() => {
   try {
-    const thread = document.getElementsByClassName(CLASS_OBJ.thread)[0];
+    const thread = document.querySelector(SELECTOR_OBJ.thread);
 
     if (
       !thread ||
       thread.isEqualNode(prevThread) ||
-      thread.getElementsByClassName(CLASS_OBJ.isLoading).length === 1
+      thread.querySelector(SELECTOR_OBJ.isLoading)
     )
       return;
 
     prevThread = thread.cloneNode(true);
 
-    const messages = thread.getElementsByClassName(CLASS_OBJ.message);
+    const messages = thread.querySelectorAll(SELECTOR_OBJ.message);
 
     if (messages.length === 0) return;
 
