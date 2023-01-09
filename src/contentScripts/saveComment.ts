f284ec96e64d15fcc89bba9bf204f24bd67d3cfd
kazizi55 <kaz.redhot1201@gmail.com>
2023-01-09 13:06:12 +0900

    feat: make comments flow when not opening comment section

--- src/contentScripts/saveComment.ts
+++ src/contentScripts/saveComment.ts
@@ -1,16 +1,48 @@
 let prevThread: Node;
 
+let prevPopup: Node;
+
 const CHAT_SELECTOR_BASE =
-  "#ow3 > div.T4LgNb > div > div[jsmodel='BA3Upd d5LS6d'] > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6";
+  "#ow3 > div.T4LgNb > div > div[jsmodel='BA3Upd d5LS6d'] > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b";
 
 const CHAT_SELECTOR_OBJ = {
-  thread: CHAT_SELECTOR_BASE,
-  message: `${CHAT_SELECTOR_BASE}> div > div.Zmm6We > div`,
+  container: CHAT_SELECTOR_BASE,
+  thread: `${CHAT_SELECTOR_BASE} > div.hWX4r > div > div.z38b6`,
+  message: `${CHAT_SELECTOR_BASE} > div.hWX4r > div > div.z38b6 > div > div.Zmm6We > div`,
 } as const;
 
 const CHAT_CLASS_OBJ = {
   isLoading: "gYckH",
+  isHidden: "qdulke",
 } as const;
+
+const POPUP_MESSAGE_SELECTOR =
+  "#ow3 > div.T4LgNb > div > div[jsmodel='BA3Upd d5LS6d'] > div.crqnQb > div.J0M6X.nulMpf.Didmac.sOkDId > div.hEr73c.nTlZFe.P9KVBf > div.bY2KB.eISbSc > div[jsname='JDWQFb'] > button.L4MuL > div.ZuRxkd > div.cOEZgf.XkylE > div.LpG93b.xtO4Tc";
+
+const extractMessageFromPopup = (popup: Element | null): string | undefined => {
+  if (!popup || popup.isEqualNode(prevPopup)) return;
+  prevPopup = popup.cloneNode(true);
+
+  return popup.innerHTML;
+};
+
+const extractMessageFromThread = (
+  thread: Element | null
+): string | undefined => {
+  if (!thread || thread.isEqualNode(prevThread)) return;
+
+  prevThread = thread.cloneNode(true);
+
+  const messageNodes = thread.querySelectorAll(CHAT_SELECTOR_OBJ.message);
+
+  if (messageNodes.length === 0) return;
+
+  const messageNode = messageNodes[messageNodes.length - 1];
+
+  if (messageNode.classList.contains(CHAT_CLASS_OBJ.isLoading)) return;
+
+  return messageNode.innerHTML;
+};
 
 const observer = new MutationObserver(async () => {
   try {
@@ -20,21 +52,16 @@ const observer = new MutationObserver(async () => {
 
     if (!isEnabledStreaming) return;
 
+    const popup = document.querySelector(POPUP_MESSAGE_SELECTOR);
+    const container = document.querySelector(CHAT_SELECTOR_OBJ.container);
     const thread = document.querySelector(CHAT_SELECTOR_OBJ.thread);
 
-    if (!thread || thread.isEqualNode(prevThread)) return;
+    const message =
+      container && !container.classList.contains(CHAT_CLASS_OBJ.isHidden)
+        ? extractMessageFromThread(thread)
+        : extractMessageFromPopup(popup);
 
-    prevThread = thread.cloneNode(true);
-
-    const messageNodes = thread.querySelectorAll(CHAT_SELECTOR_OBJ.message);
-
-    if (messageNodes.length === 0) return;
-
-    const messageNode = messageNodes[messageNodes.length - 1];
-
-    if (messageNode.classList.contains(CHAT_CLASS_OBJ.isLoading)) return;
-
-    const message = messageNode.innerHTML;
+    if (!message) return;
 
     chrome.runtime.sendMessage({
       method: "setComment",
